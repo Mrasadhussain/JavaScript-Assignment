0b753ddc9df010081c2b62f7008e3ae961390376
Mrasadhussain <asadsmit11@gmail.com>
2024-02-22 22:49:46 +0500

    index file created

---
+++ Chapter-0/app.js
@@ -0,0 +1,53 @@
+// alert
+
+alert('Error! Please enter a valid password.');
+alert('Welcome to JS Land...\nHappy Coding!');
+alert('Welcome to JS Land...');
+alert('Happy Coding! \n Prevent the page from creating additional dialogs');
+alert("Hello... I can run JS through my web browser's console");
+
+
+
+
+// alert('My Biggest Dream!');
+
+
+var firstName = 'Asad';
+var middleName = 'Hussain';
+var lastName = 'Jaffery';
+var dateOfBirth = '23-3-1991';
+var age = 11*3;
+var dream = 'web developer'
+var say = 'Inshallah tallah'
+var add = 23
+
+console.log(firstName)
+console.log(middleName)
+console.log(lastName)
+console.log(dateOfBirth)
+console.log(age)
+
+console.log('Hello my name is '+ firstName+' '+middleName+' '+lastName+ ' my date of birth is '+ dateOfBirth+ ' and i am '+ age+' year old my dream one day i am become a ' + dream+' '+say)
+
+// how to check a data type
+
+console.log(typeof(firstName))
+console.log(typeof(middleName))
+console.log(typeof(lastName))
+console.log(typeof(dateOfBirth))
+console.log(typeof(age))
+console.log(typeof(dream))
+console.log(typeof(say))
+console.log(typeof(add))
+
+console.log()
+
+
+// what is legal or illegal variable
+
+console.log('runing!')
+
+// there are two types of illegal variable
+
+var myCompanyName = 'Syatems';
+var _myCompanyName = 'Syatems';
---
+++ Chapter-2/app.js
@@ -0,0 +1,33 @@
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+alert('Asad Hussain')
+alert('33 years old')
+alert('Certified Mobile Application Development')
+
+
+// 5. Write a script to display the following alert using one JS variable:
+
+alert('PIZZA \nPIZZ \nPIZ \nPI \nP')
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+alert('My email address is example@example.com')
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+alert('I am trying to learn from the Book A smarter \n way to learn JavaScript')
+
+// 8. Write a script to display this in browser through JS
